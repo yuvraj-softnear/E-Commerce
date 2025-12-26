472e4b70ee66a02ff249f092d824bbbbc3b41aee
yuvraj-singh08 <yuvrajsinghwahla08cs@gmail.com>
2025-12-26 19:45:42 +0530

    changed default port

--- server/app.js
+++ server/app.js
@@ -71,7 +71,7 @@ app.use("/api/order", orderRouter);
 app.use("/api/customize", customizeRouter);
 
 // Run Server
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log("Server is running on ", PORT);
 });
